7a6a319b1fb2104fc84aa49693797200e62d75ee
pedrokpdo <pedromotoehm@gmail.com>
2022-06-24 18:36:40 -0300

    intervalos no calendario

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,44 +1,59 @@
 import { Feather } from '@expo/vector-icons';
 import React from 'react';
-import { Calendar as CustomCalendar, LocaleConfig } from 'react-native-calendars'
+import { Calendar as CustomCalendar, LocaleConfig, CalendarProps } from 'react-native-calendars'
 import { useTheme } from 'styled-components';
 
 LocaleConfig.locales['pt-br'] = {
     monthNames: ['Janeiro', 'Fevereiro,', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
-    monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez' ],
+    monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
     dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sabado'],
-    dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab', ],
+    dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab',],
     today: 'Hoje'
-}    
+}
+LocaleConfig.defaultLocale = 'pt-br';
 
-    LocaleConfig.defaultLocale = 'pt-br';
+interface DayProps {
+    dateString: string;
+    day: number;
+    month: number;
+    year: number
+    timestamp: number;
+}
 
-export function Calendar() {
+
+
+export function Calendar({ markedDates, onDayPress }: CalendarProps) {
     const theme = useTheme()
     return (
-        <CustomCalendar 
-        firstDay={1}
-        headerStyle={{
-            backgroundColor: theme.colors.background_secondary,
-            borderBottomWidth: 0.5,
-            borderBottomColor: theme.colors.text_detail,
-            paddingBottom: 10,
-            marginBottom:10 
-        }}
+        <CustomCalendar
+            markingType='period'
+            markedDate={markedDates}
+            minDate={new Date()}
+            onDayPress={onDayPress}
+            firstDay={1}
+            headerStyle={{
+                backgroundColor: theme.colors.background_secondary,
+                borderBottomWidth: 0.5,
+                borderBottomColor: theme.colors.text_detail,
+                paddingBottom: 10,
+                marginBottom: 10
+            }}
 
-        theme={{
-            textDayFontFamily: theme.fonts.primary_400,
-            textDayHeaderFontFamily: theme.fonts.primary_500,
-            textDayHeaderFontSize: 10,
-            textMonthFontSize: 20,
-            textMonthFontFamily: theme.fonts.secondary_600,
-            monthTextColor: theme.colors.title,
-            arrowStyle: {
-                marginHorizontal: -15
-            }
-        }}
+            theme={{
+                textDayFontFamily: theme.fonts.primary_400,
+                textDayHeaderFontFamily: theme.fonts.primary_500,
+                textDayHeaderFontSize: 10,
+                textMonthFontSize: 20,
+                textMonthFontFamily: theme.fonts.secondary_600,
+                monthTextColor: theme.colors.title,
+                arrowStyle: {
+                    marginHorizontal: -15
+                }
+            }}
             renderArrow={(direction) =>
                 <Feather name={direction === 'left' ? 'chevron-left' : 'chevron-right'} size={24} color={theme.colors.text} />
             } />
     )
 }
+
+export {DayProps}
--- src/screens/Scheduling/index.tsx
+++ src/screens/Scheduling/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BackButton } from '../../components/BackButton';
 import { useTheme } from 'styled-components';
 import ArrowSvg from '../../assets/arrow.svg'
@@ -6,22 +6,42 @@ import { Container, Header, Title, RentalPeriod, DateInfo, DateTitle, DateValue,
 import { View } from 'react-native';
 import { StatusBar } from 'react-native';
 import { Button } from '../../components/Button';
-import { Calendar } from '../../components/Calendar';
-import {useNavigation} from '@react-navigation/native'
+import { Calendar, DayProps } from '../../components/Calendar';
+import { useNavigation } from '@react-navigation/native'
+import { generateInterval } from '../../components/Calendar/generateInterval';
 
 
 export function Scheduling() {
+    const [lastSelectedDate, setLastSelectedDate] = useState<DayProps>({} as DayProps)
+    const [markedDates, setMarkedDates] = useState({})
     const theme = useTheme()
     const navigation = useNavigation()
     function handleConfirmRental() {
         navigation.navigate('SchedulingDetails')
     }
+    function handleBack() {
+        navigation.goBack();
+    }
+
+    function handleChangeDate(date: DayProps) {
+        let start = !lastSelectedDate.timestamp ? date : lastSelectedDate;
+        let end = date
+
+        if (start.timestamp > end.timestamp) {
+            start = end
+            end = start
+        }
+        setLastSelectedDate(end)
+        const interval = generateInterval(start, end)
+        setMarkedDates(interval)
+    }
+
     return (
         <Container>
             <Header>
                 <StatusBar barStyle='light-content' translucent backgroundColor='transparent' />
-                <View style={{marginTop:24}}>
-                    <BackButton onPress={() => { }} color={theme.colors.shape} />
+                <View style={{ marginTop: 24 }}>
+                    <BackButton onPress={handleBack} color={theme.colors.shape} />
                 </View>
                 <Title>
                     Escolha uma {'\n'}
@@ -41,10 +61,13 @@ export function Scheduling() {
                 </RentalPeriod>
             </Header>
             <Content>
-                <Calendar/>
+                <Calendar
+                    markedDates={markedDates}
+                    onDayPress={handleChangeDate}
+                />
             </Content>
             <Footer>
-                <Button onPress={handleConfirmRental} title='Confirmar'/>
+                <Button onPress={handleConfirmRental} title='Confirmar' />
             </Footer>
         </Container>
     )
