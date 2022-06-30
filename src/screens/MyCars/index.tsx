f944433c87520f7e5f2e1832ce58acabda0df511
pedrokpdo <pedromotoehm@gmail.com>
2022-06-30 11:14:42 -0300

    exibindo carros agendados

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,29 +1,67 @@
+import { useNavigation } from '@react-navigation/native'
 import React, { useEffect, useState } from 'react'
+import { FlatList, StatusBar, View } from 'react-native'
+import { useTheme } from 'styled-components'
+import { BackButton } from '../../components/BackButton'
+import { CardCar } from '../../components/CardCar'
 import { api } from '../../services/api'
-import { Container } from './styles'
+import { Container, Content, Header, SubTitle, Title, Appointments, AppointmentsQuatity, AppointmentsTitle } from './styles'
 
 export function MyCars() {
     const [cars, setCars] = useState([])
     const [loading, setLoading] = useState(true)
 
+    const theme = useTheme()
+
+    const navigation = useNavigation()
+
+    function handleBack() {
+        navigation.goBack()
+    }
+
     useEffect(() => {
-        async function fetchCars(){
+        async function fetchCars() {
             try {
                 const response = await api.get('schedules_byuser?user_id=1')
                 setCars(response.data)
             } catch (error) {
                 console.log(error)
             }
-            finally{
+            finally {
                 setLoading(false)
             }
         }
         fetchCars()
-    },[])
+    }, [])
 
     return (
         <Container>
-
+            <Header>
+                <StatusBar barStyle='light-content' translucent backgroundColor='transparent' />
+                <View style={{ marginTop: 24 }}>
+                    <BackButton onPress={handleBack} color={theme.colors.shape} />
+                </View>
+                <Title>
+                    Escolha uma {'\n'}
+                    data de início e {'\n'}
+                    fim do aluguel. {'\n'}
+                </Title>
+                <SubTitle>Conforto, segurança e praticidade</SubTitle>
+            </Header>
+            <Content>
+                <Appointments>
+                    <AppointmentsTitle>Agendamentos Feitos</AppointmentsTitle>
+                    <AppointmentsQuatity>05</AppointmentsQuatity>
+                </Appointments>
+                <FlatList
+                    data={cars}
+                    keyExtractor={item => item.id}
+                    showsVerticalScrollIndicator={false}
+                    renderItem={({ item }) => (
+                        <CardCar data={item.car}/>
+                    )}
+                />
+            </Content>
         </Container>
     )
 }
--- src/screens/MyCars/styles.ts
+++ src/screens/MyCars/styles.ts
@@ -1,5 +1,57 @@
+import { getStatusBarHeight } from 'react-native-iphone-x-helper'
+import { RFValue } from 'react-native-responsive-fontsize'
 import styled from 'styled-components/native'
 
 export const Container = styled.View`
-    
+    flex: 1;
+    align-items: center;
+    background-color: ${({ theme }) => theme.colors.primary};
+`
+
+export const Header = styled.View`
+    width: 100%;
+    height: 360px;
+    background-color: ${({ theme }) => theme.colors.header};
+    justify-content: center;
+    padding: 25px;
+    padding-top: ${getStatusBarHeight()}px;
+`
+
+export const Title = styled.Text`
+    color: ${({ theme }) => theme.colors.shape};
+    font-family: ${({ theme }) => theme.fonts.secondary_600};
+    font-size: ${RFValue(34)}px;
+    margin-top: 24px;
+`
+export const SubTitle = styled.Text`
+    color: ${({ theme }) => theme.colors.shape};
+    font-family: ${({ theme }) => theme.fonts.secondary_400};
+    font-size: ${RFValue(15)}px;
+    margin-top: 24px;
+`
+
+export const Content = styled.View`
+    width: 100%;
+    flex: 1;
+    padding: 0 16px;
+`
+
+export const Appointments = styled.View`
+    width: 100%;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items:center;
+    padding: 24px 0;
+`
+
+export const AppointmentsTitle = styled.Text`
+    color: ${({ theme }) => theme.colors.text};
+    font-family: ${({ theme }) => theme.fonts.primary_400};
+    font-size: ${RFValue(15)}px;
+`
+
+export const AppointmentsQuatity = styled.Text`
+    color: ${({ theme }) => theme.colors.title};
+    font-family: ${({ theme }) => theme.fonts.primary_500};
+    font-size: ${RFValue(15)}px;
 `
