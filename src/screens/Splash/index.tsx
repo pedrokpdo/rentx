02f872defb4506c56519b8ebace268eb7bbb970d
pedrokpdo <pedromotoehm@gmail.com>
2022-06-30 14:24:46 -0300

    primeira animação

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,12 +1,34 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { StyleSheet, Text } from 'react-native'
 import { Container } from './styles'
+import Animated, { useSharedValue, useAnimatedStyle } from 'react-native-reanimated'
+import { Button } from 'react-native'
 
 export function Splash() {
+    const animation = useSharedValue(0);
+    const animatedStyles = useAnimatedStyle(() => {
+        return {
+            transform: [
+                {translateX: animation.value}
+            ]
+        }
+    })
+    function handleAnimationPosition() {
+        animation.value = Math.random() * 500
+    }
 
     return (
         <Container>
-       
+            <Animated.View style={[styles.box, animatedStyles]} />
+            <Button title='Mover' onPress={handleAnimationPosition}/>
         </Container>
     )
 }
+
+const styles = StyleSheet.create({
+    box: {
+        width: 100,
+        height: 100,
+        backgroundColor: 'red'
+    }
+})
