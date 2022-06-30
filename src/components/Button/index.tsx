b38f96d74136eeec0469a80ce4f478435861d249
pedrokpdo <pedromotoehm@gmail.com>
2022-06-30 12:01:33 -0300

    ajustes no botão, fim do modulo

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,6 +5,7 @@ import { Container, Title } from './styles';
 interface Props {
     title: string;
     color?: string;
+    onPress: any;
 }
 
 export function Button({ title, color, onPress }: Props) {
