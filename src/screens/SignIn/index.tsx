3efb9c56466108d41ed6e84ef1cb1abfcefda130
pedrokpdo <pedromotoehm@gmail.com>
2022-07-04 10:22:46 -0300

    tipando icons

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Container, Header, Title, SubTitle, Footer } from './styles'
+import { Container, Header, Title, SubTitle, Footer, Form } from './styles'
 import { StatusBar } from 'react-native'
 import { Button } from '../../components/Button'
 import { useTheme } from 'styled-components'
+import { Input } from '../../components/Input'
 
 
 export function SignIn() {
@@ -10,15 +11,18 @@ export function SignIn() {
 
     return (
         <Container>
-            <StatusBar barStyle='dark-content' backgroundColor='transparent' translucent/>
+            <StatusBar barStyle='dark-content' backgroundColor='transparent' translucent />
             <Header>
                 <Title>Estamos {`\n`}quase lá</Title>
                 <SubTitle>Faça seu login para começar{`\n`}uma experiencia incrivel.</SubTitle>
-                
+
             </Header>
+            <Form>
+                <Input iconName='mail'/>
+            </Form>
             <Footer>
-                <Button title='login' onPress={() => {}}/>
-                <Button title='Criar conta Gratuita' onPress={() => {}} colorText={theme.colors.shape_dark} color={theme.colors.background_secondary}/>
+                <Button title='login' onPress={() => { }} />
+                <Button title='Criar conta Gratuita' onPress={() => { }} colorText={theme.colors.shape_dark} color={theme.colors.background_secondary} />
             </Footer>
         </Container>
     )
