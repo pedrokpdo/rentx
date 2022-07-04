2f774662a15ea7a0bbb3b7dd581352feeb514248
pedrokpdo <pedromotoehm@gmail.com>
2022-07-04 11:32:27 -0300

    acessibilidade em formulário

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Container, Header, Title, SubTitle, Footer, Form } from './styles'
-import { StatusBar } from 'react-native'
+import { StatusBar, KeyboardAvoidingView, Keyboard, TouchableWithoutFeedback } from 'react-native'
 import { Button } from '../../components/Button'
 import { useTheme } from 'styled-components'
 import { Input } from '../../components/Input'
@@ -11,21 +11,25 @@ export function SignIn() {
     const theme = useTheme();
 
     return (
-        <Container>
-            <StatusBar barStyle='dark-content' backgroundColor='transparent' translucent />
-            <Header>
-                <Title>Estamos {`\n`}quase lá</Title>
-                <SubTitle>Faça seu login para começar{`\n`}uma experiencia incrivel.</SubTitle>
+        <KeyboardAvoidingView behavior='position' enabled>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <Container>
+                    <StatusBar barStyle='dark-content' backgroundColor='transparent' translucent />
+                    <Header>
+                        <Title>Estamos {`\n`}quase lá</Title>
+                        <SubTitle>Faça seu login para começar{`\n`}uma experiencia incrivel.</SubTitle>
 
-            </Header>
-            <Form>
-                <Input iconName='mail' placeholder='E-mail' keyboardType='email-address' autoCapitalize='none' autoCorrect={false}/>
-                <PasswordInput iconName='lock' placeholder='Senha'/>
-            </Form>
-            <Footer>
-                <Button title='login' onPress={() => { }} />
-                <Button title='Criar conta Gratuita' onPress={() => { }} colorText={theme.colors.shape_dark} color={theme.colors.background_secondary} />
-            </Footer>
-        </Container>
+                    </Header>
+                    <Form>
+                        <Input iconName='mail' placeholder='E-mail' keyboardType='email-address' autoCapitalize='none' autoCorrect={false} />
+                        <PasswordInput iconName='lock' placeholder='Senha' />
+                    </Form>
+                    <Footer>
+                        <Button title='login' onPress={() => { }} />
+                        <Button title='Criar conta Gratuita' onPress={() => { }} colorText={theme.colors.shape_dark} color={theme.colors.background_secondary} />
+                    </Footer>
+                </Container>
+            </TouchableWithoutFeedback >
+        </KeyboardAvoidingView>
     )
 }
